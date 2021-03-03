f2762bb2f5fd88cfe22b30ec2f56fbf22434d3b8
Brett Gibson <brettgibson5@gmail.com>
2021-03-02 21:38:37 -0600

    alex edits

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="container">
-    <header class="sm:p-4 p-2 flex justify-between items-center mb-2">
+    <header class="sm:flex hidden p-4 pb-0 flex justify-between items-center">
       <h1><img src="/img/logo.png" class="h-16 md:h-32 mr-1" alt="Ryan Ditty" /></h1>
       <h2 class="md:text-xl sm:text-md text-sm leading-tight uppercase text-right"><strong>A Celebration of Life</strong><br />March 6, 2021 // 3PM<br />C3 Los Angeles</h2>
     </header>
-    <Swiper :options="swiperOption" class="mx-auto relative" ref="swiper">
-      <div class="swiper-slide flex items-center md:pb-12 pb-8 px-8 pt-2" :key="banner" v-for="banner in banners">
-        <!-- Render original HTML in server, render Swiper in browser (client) -->
-        <img class="mx-auto max-h-full w-auto shadow-xl" :src="banner" />
+    <div class="relative">
+      <Swiper :options="swiperOption" class="mx-auto relative" ref="mySwiper" @slideChange="changeSwiperIndex">
+        <div class="swiper-slide flex items-center pb-20 px-8 pt-4" :key="banner" v-for="banner in banners">
+          <!-- Render original HTML in server, render Swiper in browser (client) -->
+          <img class="mx-auto max-h-full w-auto shadow-xl" :src="banner" />
+        </div>
+      </Swiper>
+      <div class="absolute bottom-0 z-10 w-full flex items-center justify-center">
+        <button class="swiper-prev bg-gray-400 rounded-full p-4 inline-block m-2" slot="button-prev" @click="prev()" :disabled="activeIndex === 0">
+          <img src="/img/arrow.svg" class="w-4 pointer-events-none transform rotate-180" />
+        </button>
+        <div class="counter mx-4">{{ activeIndex + 1}} / {{banners.length}}</div>
+        <button class="swiper-next bg-gray-400 rounded-full p-4 inline-block m-2" slot="button-next" @click="next()" :disabled="activeIndex + 1 === banners.length">
+          <img src="/img/arrow.svg" class="w-4 pointer-events-none" />
+        </button>
       </div>
-    </Swiper>
-    <div class="text-center">
-      <button class="swiper-prev bg-gray-300 rounded-full p-4 inline-block m-2" slot="button-prev" @click="prev()">
-        <img src="/img/arrow.svg" class="w-4 pointer-events-none transform rotate-180" />
-      </button>
-      <div class="swiper-pagination"></div>
-      <button class="swiper-next bg-gray-300 rounded-full p-4 inline-block m-2" slot="button-next" @click="next()">
-        <img src="/img/arrow.svg" class="w-4 pointer-events-none" />
-      </button>
     </div>
   </div>
 </template>
@@ -29,6 +31,7 @@ export default {
   components: { Swiper },
   data() {
     return {
+      activeIndex: 0,
       banners: ["/img/slide1.jpg", "/img/slide2.jpg", "/img/slide3.jpg", "/img/slide4.jpg"],
       swiperOption: {
         slidesPerView: 1,
@@ -46,11 +49,14 @@ export default {
   },
   methods: {
 		prev() {
-			this.$refs.swiper.$swiper.slidePrev();
+			this.$refs.mySwiper.$swiper.slidePrev();
 		},
 		next() {
-			this.$refs.swiper.$swiper.slideNext();
-		}
+			this.$refs.mySwiper.$swiper.slideNext();
+		},
+    changeSwiperIndex () {
+      this.activeIndex = this.$refs.mySwiper.$swiper.activeIndex;
+    }
 	}
 };
 </script>
@@ -61,17 +67,26 @@ h2 {
   letter-spacing: 3px;
 }
 .swiper-slide {
-  height: calc(100vh - 240px);
+  height: calc(100vh - 160px);
   min-height: 400px;
+}
+.swiper-prev[disabled="disabled"],
+.swiper-next[disabled="disabled"] {
+  cursor: default;
+  opacity: 0.4;
+}
+.counter {
+  color: #00aeff;
 }
 
 @media only screen and (max-width: 768px) {
   h2 {
     letter-spacing: 2px;
   }
+}
+@media only screen and (max-width: 640px) {
   .swiper-slide {
-    height: calc(100vh - 160px);
+    height: 100vh;
   }
 }
-
 </style>
